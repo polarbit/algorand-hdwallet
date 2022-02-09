0655d67d9d81ab790202dbb8d6bf9df98a3d770c
Şafak Ulusoy <safakulusoy@gmail.com>
2022-02-09 23:49:05 +0000

    ed25519 bip32 wallet derivation implemented (needs refactor)

--- hdwallet/hdwallet.go
+++ hdwallet/hdwallet.go
@@ -7,6 +7,9 @@ import (
 	"encoding/binary"
 	"encoding/hex"
 	"errors"
+
+	"github.com/polarbit/algorand-hdwallet/bip32path"
+	"github.com/polarbit/algorand-hdwallet/utils"
 )
 
 const (
@@ -15,94 +18,133 @@ const (
 	CURVE_NIST256P1 = "Nist256p1 seed"
 )
 
-type HdWalletOptions struct {
-	Curve           string
-	FnParseMnemonic func(m string) ([]byte, error)
-}
+type FnParseMnemonic func(m string) ([]byte, error)
 
 type ExtendedKey struct {
-	Key       []byte
-	ChainCode string
+	Key         []byte
+	ChainCode   []byte
+	PrivateKey  []byte
+	PublicKey   []byte
+	Fingerprint []byte
+	ParentKey   *ExtendedKey
 }
 
-func (o *HdWalletOptions) GenerateMasterKey(seed []byte) ([]byte, []byte, error) {
+func GenerateMasterKey(curve string, seed []byte) (xkey *ExtendedKey, err error) {
 	if len(seed) < 16 || len(seed) > 64 {
-		return nil, nil, errors.New("Invalid  seed length")
+		return nil, errors.New("Invalid  seed length")
 	}
 
-	if o.Curve != CURVE_ED25519 {
-		return nil, nil, errors.New("Curve not supported")
+	// TODO:
+	if curve != CURVE_ED25519 {
+		panic("Curve not supported")
 	}
 
-	salt := []byte(o.Curve)
+	salt := []byte(curve)
 	hash := hmac.New(sha512.New, salt)
-	_, err := hash.Write(seed[:])
+	_, err = hash.Write(seed[:])
 	if err != nil {
-		panic(err)
+		return
 	}
 
 	I := hash.Sum(nil)
-	IL := I[:32]
-	IR := I[32:]
 
-	if o.Curve != CURVE_ED25519 {
+	if curve != CURVE_ED25519 {
 		// TODO: Check invalid values
 		// If curve is not ed25519 and IL is 0 or ≥ n (invalid key)
 		// Set S := I and continue at step 2.
 	}
 
-	return IL, IR, nil
+	xkey, err = BuildExtendedKey(nil, I[:32], I[32:])
+
+	return
 }
 
-func (o *HdWalletOptions) GenerateMasterKeyFromHexSeed(seed string) ([]byte, []byte, error) {
+func GenerateMasterKeyFromHexSeed(curve string, seed string) (*ExtendedKey, error) {
 	key, err := hex.DecodeString(seed)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
-	return o.GenerateMasterKey(key)
+	return GenerateMasterKey(curve, key)
 }
 
-func (o *HdWalletOptions) GenerateMasterKeyFromMnemonic(mnemonic string) ([]byte, []byte, error) {
-	key, err := o.FnParseMnemonic(mnemonic)
+func GenerateMasterKeyFromMnemonic(curve string, fnParseMnemonic FnParseMnemonic, mnemonic string) (*ExtendedKey, error) {
+	key, err := fnParseMnemonic(mnemonic)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
-	return o.GenerateMasterKey(key)
+	return GenerateMasterKey(curve, key)
 }
 
-func CKD(key []byte, chainCode []byte, ix uint32) ([]byte, []byte) {
+func GenerateAccount(path *bip32path.Bip32Path, masterXKey *ExtendedKey) (xkey *ExtendedKey, err error) {
+	xkey = masterXKey
+
+	for _, s := range path.Segments {
+		xkey, err = CKD(xkey, s.Value)
+	}
+
+	return
+}
+
+func CKD(xparent *ExtendedKey, ix uint32) (xkey *ExtendedKey, err error) {
 	vbuf := make([]byte, 4)
 	binary.BigEndian.PutUint32(vbuf, ix)
+
+	key := xparent.Key
+	chainCode := xparent.ChainCode
 
 	buf := make([]byte, 1)
 	buf = append(buf, key...)
 	buf = append(buf, vbuf...)
 
 	hash := hmac.New(sha512.New, chainCode)
-	_, err := hash.Write(buf)
+	_, err = hash.Write(buf)
 	if err != nil {
-		// TODO: Do not panic
-		panic(err)
+		return
 	}
 
 	I := hash.Sum(nil)
-	IL := I[:32]
-	IR := I[32:]
+	xkey, err = BuildExtendedKey(xparent, I[:32], I[32:])
 
-	return IL, IR
+	return
 }
 
-func GetPublicKey(key []byte) string {
+func GetCurveKeyPair_Ed255129(key []byte) ([]byte, []byte, error) {
 	priv := ed25519.NewKeyFromSeed(key)
 	pub := priv.Public()
 
 	buf := make([]byte, 32)
 	copy(buf, pub.(ed25519.PublicKey))
 
-	buf = append(make([]byte, 1), buf...)
-	hexPub := hex.EncodeToString(buf)
+	pubKey := append(make([]byte, 1), buf...)
 
-	return hexPub
+	return priv, pubKey, nil
+}
+
+func BuildExtendedKey(xparent *ExtendedKey, key []byte, chainCode []byte) (*ExtendedKey, error) {
+	priv, pub, err := GetCurveKeyPair_Ed255129(key)
+	if err != nil {
+		return nil, err
+	}
+
+	xkey := &ExtendedKey{
+		Key:        key,
+		ChainCode:  chainCode,
+		PrivateKey: priv,
+		PublicKey:  pub,
+		ParentKey:  xparent,
+	}
+
+	if xparent != nil {
+		keyId, err := utils.Hash160(xparent.PublicKey)
+		if err != nil {
+			return nil, err
+		}
+		xkey.Fingerprint = keyId[:4]
+	} else {
+		xkey.Fingerprint = []byte{0, 0, 0, 0}
+	}
+
+	return xkey, nil
 }
--- main.go
+++ main.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"crypto/ed25519"
 	"encoding/hex"
 	"fmt"
 
-	"github.com/polarbit/algorand-hdwallet/algorand"
 	"github.com/polarbit/algorand-hdwallet/bip32path"
 	"github.com/polarbit/algorand-hdwallet/hdwallet"
-	"github.com/polarbit/algorand-hdwallet/utils"
 )
 
 const testVector2Seed = "000102030405060708090a0b0c0d0e0f"
-const testVector2Path = "m/0'"
+const testVector2Path = "m/0'/1'/2'/2'/1000000000'"
 
 const (
 	ED25519_CURVE   = "ed25519 seed"
@@ -32,51 +29,15 @@ func testIt() {
 		panic(err)
 	}
 
-	wallet := hdwallet.HdWalletOptions{
-		Curve:           hdwallet.CURVE_ED25519,
-		FnParseMnemonic: algorand.GetSeedFromMnemonic,
-	}
-
-	mkey, mchaincode, err := wallet.GenerateMasterKeyFromHexSeed(testVector2Seed)
+	masterXKey, err := hdwallet.GenerateMasterKeyFromHexSeed(hdwallet.CURVE_ED25519, testVector2Seed)
 	if err != nil {
 		panic(err)
 	}
 
-	key, chaincode := generateAccount(accountPath, mkey, mchaincode)
+	xkey, err := hdwallet.GenerateAccount(accountPath, masterXKey)
 
-	fmt.Println("chaincode: ", hex.EncodeToString(chaincode))
-	fmt.Println("private: ", hex.EncodeToString(key))
-
-	hexPub := getPublicKey(key)
-
-	fmt.Println("public: ", hexPub)
-
-	mpub := getPublicKey(mkey)
-	mpubbytes, _ := hex.DecodeString(mpub)
-	keyID, _ := utils.Hash160(mpubbytes)
-	fingerPrintPub := hex.EncodeToString(keyID[:4])
-	fmt.Println("fingerprint: ", fingerPrintPub)
-}
-
-func generateAccount(path *bip32path.Bip32Path, masterKey []byte, masterChainCode []byte) ([]byte, []byte) {
-	key, chainCode := masterKey, masterChainCode
-
-	for _, s := range path.Segments {
-		key, chainCode = hdwallet.CKD(key, chainCode, s.Value)
-	}
-
-	return key, chainCode
-}
-
-func getPublicKey(key []byte) string {
-	priv := ed25519.NewKeyFromSeed(key)
-	pub := priv.Public()
-
-	buf := make([]byte, 32)
-	copy(buf, pub.(ed25519.PublicKey))
-
-	buf = append(make([]byte, 1), buf...)
-	hexPub := hex.EncodeToString(buf)
-
-	return hexPub
+	fmt.Println("chaincode: ", hex.EncodeToString(xkey.ChainCode))
+	fmt.Println("private: ", hex.EncodeToString(xkey.PrivateKey))
+	fmt.Println("public: ", hex.EncodeToString(xkey.PublicKey))
+	fmt.Println("fingerprint: ", hex.EncodeToString(xkey.Fingerprint))
 }
